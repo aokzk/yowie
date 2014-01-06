5a531890f720535c05ea99e0aea5ea687ad62d18
tokzk <t@okzk.org>
2014-01-06 16:41:46 +0900

    Initial version
    rails new yowie -T --skip-bundle -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yowie::Application.config.secret_key_base = '2e7d48b421b81cd5d744a65fa51964d527141f3f075704c352c19fe99584366eb891ddbff5a78bbc9ee99f959eeeba3556dd293ca240bda12a7de6615ffd12d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yowie::Application.config.session_store :cookie_store, key: '_yowie_session'
